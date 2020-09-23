bc51367c9e250fec80f545f070dfef1172f08c37
Carl Allan Patrick Mercado <carl@192.168.68.105>
2020-09-23 20:57:46 +0800

    sadasdaa

--- main.cpp
+++ main.cpp
@@ -1,8 +1,29 @@
+#include <windows.h>
 #include <iostream>
 
-using namespace std;
+using namespace std ;
 
-int main() {
-    cout << "Hello, World!" << endl;
-    return 0;
+int main()
+{
+    char ownPth[MAX_PATH];
+
+    // When NULL is passed to GetModuleHandle, the handle of the exe itself is returned
+    HMODULE Handler = GetModuleHandle(NULL);
+    cout << Handler;
+//    if (Handler != NULL)
+//    {
+//        // Use GetModuleFileName() with module handle to get the path
+//        GetModuleFileName(Handler, ownPth, (sizeof(ownPth)));
+//       // cout << ownPth << endl ;
+//        system("PAUSE");
+//        return 0;
+//    }
+//    else
+//    {
+//        cout << "Module handle is NULL" << endl ;
+//        system("PAUSE");
+//        return 0;
+//    }
+
+return 0;
 }
